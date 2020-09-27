45464ef1c6f741ab2de61ed14a4004191e28346a
priyanshu71 <bharga_841920@student.nitw.ac.in>
2020-09-27 19:25:37 +0530

    Updates

---
+++ precache-manifest.05376e7c76c5789a01025c63130c9ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d40c0b1ae53cafccf7980774174eca7",
+    "url": "/Search-Movies/index.html"
+  },
+  {
+    "revision": "a3320691d48bf4617471",
+    "url": "/Search-Movies/static/css/main.60825f79.chunk.css"
+  },
+  {
+    "revision": "b08892ca4d14882543e5",
+    "url": "/Search-Movies/static/js/2.1a6b1908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Search-Movies/static/js/2.1a6b1908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3320691d48bf4617471",
+    "url": "/Search-Movies/static/js/main.2292e617.chunk.js"
+  },
+  {
+    "revision": "93f81a5a87f6077002f9",
+    "url": "/Search-Movies/static/js/runtime-main.f197e809.js"
+  }
+]);
